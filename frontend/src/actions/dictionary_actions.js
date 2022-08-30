ee87646e52dd6236a5d8e27397dc75a31f3f4108
Irene Martinez <imartinez921@gmail.com>
2022-08-30 10:54:43 -0400

    Finished fetchDefinitions api util function to retrieve data

--- frontend/src/actions/dictionary_actions.js
+++ frontend/src/actions/dictionary_actions.js
@@ -1,22 +1,21 @@
 import { fetchDefinitions } from "../util/dictionary_api_util";
 
-export const RECEIVE_WORD = "RECEIVE_WORD";
+export const RECEIVE_DEFINITIONS = "RECEIVE_DEFINITIONS";
 export const RECEIVE_DICTIONARY_ERRORS = "RECEIVE_DICTIONARY_ERRORS";
 
-const receiveWord = (word) => ({
-    type: RECEIVE_WORD,
-    word,
+const receiveDefinitions = (definitions) => ({
+    type: RECEIVE_DEFINITIONS,
+    definitions, // this is an array
 });
 
 const receiveDictionaryErrors = (errors) => ({
-  type: RECEIVE_DICTIONARY_ERRORS,
-  errors: errors,
-})
+    type: RECEIVE_DICTIONARY_ERRORS,
+    errors: errors,
+  })
 
 export const lookupWord = query => dispatch => {
-  // debugger;
   return (
   fetchDefinitions(query)
-  .then( res => dispatch( receiveWord(res.data) ),
+  .then( data => dispatch( receiveDefinitions(data) ),
   err => (dispatch( receiveDictionaryErrors(err.responseJSON) )))
   )};
--- frontend/src/reducers/dictionary_reducer.js
+++ frontend/src/reducers/dictionary_reducer.js
@@ -1,14 +1,16 @@
 import {
-  RECEIVE_WORD,
+  RECEIVE_DEFINITIONS,
 } from "../actions/dictionary_actions.js";
   
   const dictionaryReducer = (state = {},action) => {
     Object.freeze(state);
-
     let newState = Object.assign({}, state);
+
     switch (action.type) {
-      case RECEIVE_WORD:
-        console.log('WORD.DATA', action.word.data)
+      case RECEIVE_DEFINITIONS:
+        action.map( def => {
+          def.attributionText: "from WordNet 3.0 Copyright 2006 by Princeton University. All rights reserved."
+        })
         newState.word = action.word.data;
         return newState;
       default:
