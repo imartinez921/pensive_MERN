6890406ab41d49a7b9c1a098dbb8f9e5822d35e2
Irene Martinez <imartinez921@gmail.com>
2022-08-30 22:04:50 -0400

    Add query to the queries slice of state (array) to track recent lookups (queryReducer, query actions)

--- frontend/src/actions/dictionary_actions.js
+++ frontend/src/actions/dictionary_actions.js
@@ -1,8 +1,8 @@
 import { fetchDefinitions } from "../util/dictionary_api_util";
 
-export const KEEP_QUERY = "KEEP_QUERY"
 export const RECEIVE_DEFINITIONS = "RECEIVE_DEFINITIONS";
 export const RECEIVE_DICTIONARY_ERRORS = "RECEIVE_DICTIONARY_ERRORS";
+
 
 const receiveDefinitions = (definitions) => {
   console.log('RECEIVING DEFINITIONS', definitions)
@@ -11,28 +11,21 @@ const receiveDefinitions = (definitions) => {
   definitions, // this is an array
 });}
 
-const keepQuery = (word) => ({
-  type: KEEP_QUERY,
-  word,
-})
-
 const receiveDictionaryErrors = (errors) => ({
     type: RECEIVE_DICTIONARY_ERRORS,
     errors: errors,
   })
 
 export const lookupWord = query => dispatch => {
-  debugger
-  console.log('.JSON IN ACTIONS')
-  keepQuery(query);
   return (
-  fetchDefinitions(query)
-  .then(res => res.json())
-  .then(res => dispatch( receiveDefinitions(res) ),
-  err => (dispatch( receiveDictionaryErrors(err.responseJSON) )))
+    fetchDefinitions(query)
+    .then(res => res.json())
+    .then(res => dispatch( receiveDefinitions(res) ),
+    err => (dispatch( receiveDictionaryErrors(err.responseJSON) )))
   )};
 
-// DATA is an ARRAY of definition objects
+// RES is an ARRAY of definition objects
+  // eg. query = 'bluebird'
   // attributionText: "from WordNet 3.0 Copyright 2006 by Princeton University. All rights reserved."
   // attributionUrl: "https://wordnet.princeton.edu/"
   // citations: []
